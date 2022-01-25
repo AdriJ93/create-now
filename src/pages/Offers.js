b2cbc6ef6db63ea91959d4753e1ad8ca3f7f4575
AdriJ93 <adrianjerome.wulfu@gmail.com>
2022-01-25 21:14:48 +0100

    5

--- src/pages/Offers.js
+++ src/pages/Offers.js
@@ -8,12 +8,43 @@ function Offers() {
       <div className="cards">
         <div className="leftCard">
           <h1>Einsteiger-Paket</h1>
+          <p>
+            Sendefähiger Videobeitrag (max 3 Min. Länge). Ideal für: Instagram
+            Reels, Tik Tok Clips oder als Unterstützung für größere Beiträge
+          </p>
+          <ul>
+            <li>Material sichten</li>
+            <li>Rogh Cut</li>
+            <li>Chronologische Anordnung</li>
+            <li>Ton</li>
+          </ul>
+          <h2>149,00 €</h2>
         </div>
         <div className="middle">
           <h1>Standard-Paket</h1>
+          <p>
+            Sendefähiger Videobeitrag
+            <br />
+            Ideal für: Youtube, Instagram, Commercial Ads oder TikTok
+          </p>
+          <ul>
+            <li>Material sichten</li>
+            <li>Rogh Cut</li>
+            <li>Chronologische Anordnung</li>
+            <li>Übergänge</li>
+            <li>Wunschmusik importieren</li>
+            <li>Color Correction</li>
+            <li>Effekte</li>
+          </ul>
+          <h2>299,00 €</h2>
         </div>
         <div className="rightCard">
           <h1>Premium-Paket</h1>
+          <p>
+            Videobeitrag z.B. für Hochzeitvideos, Fernsehbeiträge,
+            Werbekampagnen und Flogeaufträge
+          </p>
+          <h2>Preis individell nach Aufwand</h2>
         </div>
       </div>
     </div>
